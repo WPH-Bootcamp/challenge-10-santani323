f9ff9abe23bd56012ced96a83ca66a9136dcc0dd
santani423 <santani423@gmail.com>
2026-02-03 22:45:25 +0700

    penyesuaian type blog

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,24 +1,25 @@
-/**
- * Blog Types
- * 
- * TODO: Define types sesuai dengan response dari API
- * Contoh structure (sesuaikan dengan API response yang sebenarnya):
- */
+ 
+export interface Author {
+  id: number;
+  name: string;
+  email: string;
+}
 
-// export interface BlogPost {
-//   id: string;
-//   title: string;
-//   content: string;
-//   author: string;
-//   createdAt: string;
-//   image?: string;
-//   category?: string;
-//   // ... tambahkan fields lainnya sesuai API
-// }
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  tags: string[];
+  imageUrl: string;
+  author: Author;
+  createdAt: string;
+  likes: number;
+  comments: number;
+}
 
-// export interface BlogPostListResponse {
-//   posts: BlogPost[];
-//   total: number;
-//   page: number;
-//   // ... tambahkan fields lainnya
-// }
+export interface ArticlesResponse {
+  data: Article[];
+  total: number;
+  page: number;
+  lastPage: number;
+}
