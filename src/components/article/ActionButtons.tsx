7d6bc18276999fade8f41e362bcff69f72b19776
santani423 <santani423@gmail.com>
2026-02-05 01:24:07 +0700

    penyesuian modal

--- src/components/article/ActionButtons.tsx
+++ src/components/article/ActionButtons.tsx
@@ -3,28 +3,21 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import { Article } from "@/types/blog";
+import DeleteButtons from "./DeleteButtons";
+import StatisticButtons from "./StatisticButtons";
 
 export default function ActionButtons(item: Article) {
   return (
     <div className="flex items-center gap-4 text-sm">
-      <button
-        type="button"
-        className="text-blue-600 hover:underline transition-colors"
-      >
-        Statistic
-      </button>
+      <StatisticButtons {...item} />
       <button
         type="button"
         className="text-blue-600 hover:underline transition-colors"
       >
         Edit
       </button>
-      <button
-        type="button"
-        className="text-red-500 hover:underline transition-colors"
-      >
-        Delete
-      </button>
+
+      <DeleteButtons {...item} />
     </div>
   );
 }
