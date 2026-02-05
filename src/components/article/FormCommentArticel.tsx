0aa1393808c3c698b6c348451139ad64fec1592d
santani423 <santani423@gmail.com>
2026-02-05 20:53:56 +0700

    penyesuaian form comment

--- src/components/article/FormCommentArticel.tsx
+++ src/components/article/FormCommentArticel.tsx
@@ -2,15 +2,43 @@
 import Link from "next/link";
 import { useState } from "react";
 import Button from "@/components/ui/Button";
+import { useComment } from "@/hooks/useComment";
+import { useBlogs } from "@/hooks/useBlogs";
+import { useSelector } from "react-redux";
 
 export default function FormCommentArticel() {
   const [comment, setComment] = useState("");
+
+  const { id } = useSelector((state: any) => state.article);
+
+  const { loading: loadingComment, postComment } = useComment();
+  
+    const {
+ 
+      comments,
+      articles,
+      fetchArticleDetail,
+      fetchComments,
+      fetchByUserId,
+    } = useBlogs();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    postComment(
+      {
+        content: comment,
+      },
+      id || 0,
+    );
+    setComment("");
+      fetchComments({ id });
+  }
   return (
     <>
       {/* Form */}
-      <div className="px-6 pt-4 pb-2">
+      <form className="px-6 pt-4 pb-2" onSubmit={handleSubmit}>
         <label className="block font-medium mb-2 text-sm">
-          Give your Comments
+          Give your Comments  
         </label>
         <textarea
           className="w-full border rounded-lg px-3 py-2 mb-3 text-sm resize-none focus:outline-none focus:ring-2 focus:ring-sky-500"
@@ -20,11 +48,11 @@ export default function FormCommentArticel() {
           rows={3}
         />
         <div className="flex justify-end">
-          <Button type="submit" variant="primary" align="right" onClick={() => {}}>
+          <Button type="submit" variant="primary" align="right" disabled={loadingComment}>
             Send
           </Button>
         </div>
-      </div>
+      </form>
     </>
   );
 }
